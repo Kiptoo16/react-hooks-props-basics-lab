b65001a216a67972b87421798b3490b7ef59382f
Kevin Langat <kevin.langat@student.moringaschool.com>
2024-08-08 19:15:59 +0300

    Adds src/index.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,13 @@
-//import React from "react";
-//import React from 'react';
+import React from "react";
 
-function Home(props) { 
-  // console.log(props.user);
-
+function Home(props) {
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
         {props.name} is a Web Developer from {props.city}
-      <h1 style={{ color: props.user.color }}>
-        {props.user.name} is a Web Developer from {props.user.city}
       </h1>
     </div>
   );
-  }
+}
 
 export default Home;
